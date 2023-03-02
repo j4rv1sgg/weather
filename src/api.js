8d96dff951bfdfe378d2fed6656ac32a62376437
j4rv1sgg <jrvs.inc@gmail.com>
2023-03-02 22:11:11 +0100

    First commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c8815ad7dfmsh85faa71b7eb70ccp1f21ecjsn31e5fe43672d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = "01d9b664795259b591991162b55db27d"
